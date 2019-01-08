05592081f453c86a0455b4df21d7e95c9002e1d2
akep <akeppers21@gmail.com>
2019-01-08 17:53:08 -0500

    Revert "Update replace.js"
    This reverts commit 6d86f2e5974fc4f687c28f275f1ca2d25089d9d0.

--- replace.js
+++ replace.js
@@ -5,14 +5,14 @@
 replaceInterro(document.body);
 
 function replaceInterro(node){
-    if(node.childNodes !== null){
-        for(var i = 0; i < node.childNodes.length; i++){
-            if(node.childNodes[i].nodeType == Node.TEXT_NODE){
-                var oldNode = node.childNodes[i];
-                var newNode = document.createTextNode(oldNode.nodeValue.replace(/[!?]*(\?!|!\?)+([!?])*/g , '‽'));
-                node.replaceChild(newNode, oldNode);
-            }
-            else replaceInterro(node.childNodes[i]);
+    for(var i = 0; i < node.childNodes.length; i++){
+        if(node.childNodes[i].nodeType == Node.TEXT_NODE){
+            var oldNode = node.childNodes[i];
+            var newNode = document.createTextNode(oldNode.nodeValue.replace(/[!?]*(\?!|!\?)+([!?])*/g , '‽'));
+            node.replaceChild(newNode, oldNode);
         }
+        else replaceInterro(node.childNodes[i]);
     }
 }
+
+
